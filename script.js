92ebc84c6b91bf9b05932f7f68e19bdf7f81e9b3
Joan Lora <97771396+joanlora@users.noreply.github.com>
2023-04-27 21:55:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelector('#tip-form').onchange = function(){
+
+    var bill = Number(document.getElementById('billTotal').value)
+    var tip = document.getElementById('tipInput').value
+    var people = document.getElementById('totalPeople').value
+    document.getElementById('tipOutput').innerHTML = `${tip}%`
+
+    var tipValue = bill * (tip/100)
+    var finalBill = bill + tipValue
+    var tipPerPerson = (bill * (tip/100)) / people
+
+    console.log(finalBill)
+    console.log("total per person: " + tipPerPerson)
+
+    var tipAmount = document.querySelector('#tipAmount')
+    var totalBillWithTip = document.querySelector('#totalBillWithTip')
+    var totalPerPerson = document.querySelector('#tipPerPerson')
+  
+    tipAmount.value = tipValue.toFixed(2)
+    totalBillWithTip.value = finalBill.toFixed(2)
+    totalPerPerson.value = tipPerPerson.toFixed(2)
+  
+    document.getElementById('results').style.display='block'
+  }
